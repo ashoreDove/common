e0fffd3c29816a62bf996fef160aa7b5c11864a9
hdrawery <857529511@qq.com>
2023-02-06 17:24:38 +0800

    初始化

--- tool.go
+++ tool.go
@@ -3,7 +3,13 @@ package common
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-plugins/registry/consul/v2"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -24,4 +30,40 @@ func Captcha() string {
 		code = fmt.Sprintf("%s%d", code, rand.Intn(10))
 	}
 	return code
+}
+
+//初始化
+type MicroOptions struct {
+	ConsulCfg      *config.Config
+	ConsulRegister *registry.Registry
+	DB             *gorm.DB
+}
+
+func Init() (*MicroOptions, error) {
+	//配置中心
+	consulCfg, err := GetConsulConfig("127.0.0.1", 8500, "/micro/config")
+	if err != nil {
+		return nil, err
+	}
+	//注册中心
+	consulRegister := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{"127.0.0.1:8500"}
+	},
+	)
+	//获取mysql配置
+	mysqlInfo, err := GetMysqlFromConsul(consulCfg, "mysql")
+	if err != nil {
+		return nil, err
+	}
+	db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Pwd+
+		"@tcp("+mysqlInfo.Host+":"+strconv.FormatInt(mysqlInfo.Port, 10)+
+		")/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		return nil, err
+	}
+	return &MicroOptions{
+		ConsulCfg:      &consulCfg,
+		ConsulRegister: &consulRegister,
+		DB:             db,
+	}, nil
 }
